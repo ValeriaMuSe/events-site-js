463b570d606e528f6cc3def3777faf40b7bc8b01
Vale <valeriams13@gmail.com>
2023-06-28 12:44:25 -0600

    feat(css, js): Add account link and styles

--- js/modules/config/config.js
+++ js/modules/config/config.js
@@ -4,6 +4,14 @@ function initializeButtons() {
   var eventscategories = ['Music', 'Sports', 'Business', 'Food', 'Art'];
   var defaultButton = null;
 
+  var navigationContainer = document.querySelector('.container');
+  navigationContainer.innerHTML += `
+    <div class="account__container">
+      <a class="my-account__link" href="../../../account.html">My account</a>
+      <a class="my-account__link" href="../../../account.html"><img class="arrow-right__icon" src="../../../images/arrow-right-icon.svg"></a>
+    </div>
+  `
+
   eventscategories.forEach(function (category) {
     var button = document.createElement('button');
     button.innerHTML = category;
--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -4,27 +4,6 @@ import { EventAPIProxy } from './services/eventAPI.js';
 
 const eventApi = EventAPIProxy;
 
-// function renderEvents(eventsData) {
-//   var gridContainer = document.getElementById('grid-container');
-//   if (!gridContainer) {
-//     gridContainer = document.createElement('div');
-//     gridContainer.id = 'grid-container';
-//     document.body.appendChild(gridContainer);
-//   }
-//   gridContainer.innerHTML = eventsData
-//     .map((event) => `
-//       <div class="event">
-//         <img class="event_image" src="${event.image}">
-//         <p class="event_title">${event.title}</p>
-//         <p>${formatDate(event.date)}</p>
-//         <p>${renderPrice(event.price)}</p>
-//         <p>Location: ${event.location.city}, ${event.location.state},${event.location.address} </p>
-      
-//       </div>
-//     `)
-//     .join('');
-// }
-
 function renderEvents(eventsData) {
   var gridContainer = document.getElementById('grid-container');
   if (!gridContainer) {
