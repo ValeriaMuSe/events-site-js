7087e7dca03a5e359060bed3afbd82dc30fbc15d
Stephgarro <109922003+Stephgarro@users.noreply.github.com>
2023-06-28 10:15:20 -0600

    feat(js): add new version of proxy

--- js/modules/config/config.js
+++ js/modules/config/config.js
@@ -33,3 +33,4 @@ function initializeButtons() {
 }
 
 export { initializeButtons };
+
--- js/modules/services/eventAPI.js
+++ js/modules/services/eventAPI.js
@@ -1,31 +1,65 @@
-const eventApiUrl = 'https://knassbani2.execute-api.us-east-2.amazonaws.com/events/';
+// const eventApiUrl = 'https://knassbani2.execute-api.us-east-2.amazonaws.com/events/';
 
-class EventAPI {
-  constructor() {
-    this.categories = ['music', 'art', 'food', 'business', 'sports'];
-    this.cache = {};
-  }
+// class EventAPI {
+//   constructor() {
+//     this.categories = ['music', 'art', 'food', 'business', 'sports'];
+//     this.cache = {};
+//   }
 
-  async getEventsByCategory(category) {
-    if (category in this.cache) {
-      console.log(`Using cache for category: ${category}`);
-      return this.cache[category];
-    }
+//   async getEventsByCategory(category) {
+//     if (category in this.cache) {
+//       console.log(`Using cache for category: ${category}`);
+//       return this.cache[category];
+//     }
 
-    const url = `${eventApiUrl}${category}`;
-    const response = await fetch(url);
-    const data = await response.json();
+//     const url = `${eventApiUrl}${category}`;
+//     const response = await fetch(url);
+//     const data = await response.json();
 
-    this.cache[category] = data;
+//     this.cache[category] = data;
 
-    return data;
-  }
+//     return data;
+//   }
 
-  getAllCategories() {
-    return this.categories;
-  }
-}
+//   getAllCategories() {
+//     return this.categories;
+//   }
+// }
 
+// const cache = {};
+
+// const proxyCache = new Proxy(cache, {
+//   get: async (obj, prop) => {
+//     if (obj[prop]) {
+//       console.log(`Using proxy cache for category: ${prop}`);
+//       return Reflect.get(obj, prop);
+//     } else {
+//       const data = await eventAPI.getEventsByCategory(prop);
+//       obj[prop] = data;
+//       return Reflect.get(obj, prop);
+//     }
+//   }
+// });
+
+// const eventAPI = new EventAPI();
+
+// const EventAPIProxy = {
+//   async getEventsByCategory(category) {
+//     return proxyCache[category];
+//   },
+
+//   getAllCategories() {
+//     return eventAPI.getAllCategories();
+//   },
+
+//   async getEventsByCategoryProxy(category) {
+//     return proxyCache[category];
+//   }
+// };
+
+// export { EventAPIProxy };
+
+//new version of proxy 
 const cache = {};
 
 const proxyCache = new Proxy(cache, {
@@ -34,14 +68,16 @@ const proxyCache = new Proxy(cache, {
       console.log(`Using proxy cache for category: ${prop}`);
       return Reflect.get(obj, prop);
     } else {
-      const data = await eventAPI.getEventsByCategory(prop);
+      const eventApiUrl = 'https://knassbani2.execute-api.us-east-2.amazonaws.com/events/';
+      const url = `${eventApiUrl}${prop}`;
+      const response = await fetch(url);
+      const data = await response.json();
+
       obj[prop] = data;
       return Reflect.get(obj, prop);
     }
   }
 });
-
-const eventAPI = new EventAPI();
 
 const EventAPIProxy = {
   async getEventsByCategory(category) {
@@ -49,11 +85,8 @@ const EventAPIProxy = {
   },
 
   getAllCategories() {
-    return eventAPI.getAllCategories();
-  },
-
-  async getEventsByCategoryProxy(category) {
-    return proxyCache[category];
+    const categories = ['music', 'art', 'food', 'business', 'sports'];
+    return categories;
   }
 };
 
